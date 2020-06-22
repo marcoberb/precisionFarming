2b07a56c75b1cba0a9353739e4bd6d87137a7c6f
Massimiliano Bruni <massimiliano.bruni@icloud.com>
2020-06-22 16:17:24 +0200

    Initial implementation of saving raw data to MongoDB

--- server/src/main/java/com/mammut/precisionfarming/server/ServerApplication.java
+++ server/src/main/java/com/mammut/precisionfarming/server/ServerApplication.java
@@ -2,9 +2,11 @@ package com.mammut.precisionfarming.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class ServerApplication {
 
     public static void main(String[] args) throws InterruptedException {
--- server/src/main/java/com/mammut/precisionfarming/server/SparkStreamingListener.java
+++ server/src/main/java/com/mammut/precisionfarming/server/SparkStreamingListener.java
@@ -1,11 +1,15 @@
 package com.mammut.precisionfarming.server;
 
+import com.mongodb.spark.MongoSpark;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.StorageLevels;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
@@ -14,6 +18,7 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -38,10 +43,14 @@ public class SparkStreamingListener implements Serializable {
     public void listen() throws InterruptedException {
 
         // Spark Streaming configuration
-        SparkConf sparkConf = new SparkConf().setAppName("server").setMaster("local");
-        log.info("SparkConf set for {}", sparkConf);
-        JavaStreamingContext jsc = new JavaStreamingContext(sparkConf, Durations.seconds(5));
-        log.info("JavaStreamingContext set {}", jsc);
+        SparkSession sparkSession = SparkSession.builder()
+                .master("local")
+                .appName("server")
+                .config("spark.mongodb.input.uri", "mongodb://localhost:27011/precisionFarming.raw")
+                .config("spark.mongodb.output.uri", "mongodb://localhost:27011/precisionFarming.raw")
+                .getOrCreate();
+        JavaSparkContext javaSparkContext = new JavaSparkContext(sparkSession.sparkContext());
+        JavaStreamingContext jsc = new JavaStreamingContext(javaSparkContext, Durations.seconds(5));
 
         // Kafka configuration
         List<String> topicsList = Arrays.asList(channels.split(","));
@@ -71,23 +80,53 @@ public class SparkStreamingListener implements Serializable {
 
         // Receive meteo data
         JavaDStream<String> meteoLines = meteoMessages.map(ConsumerRecord::value);
-        meteoLines.print();
+        //meteoLines.print();
 
         // Receive suolo data
         JavaDStream<String> suoloLines = suoloMessages.map(ConsumerRecord::value);
-        suoloLines.print();
+        //suoloLines.print();
 
         // Split meteo fields
-        JavaDStream<String> meteoFields = meteoLines.flatMap(line -> Arrays.asList(SEPARATOR.split(line)).iterator());
-        meteoFields.print();
+        // JavaDStream<String> meteoFields = meteoLines.flatMap(line -> Arrays.asList(SEPARATOR.split(line)).iterator());
+        meteoLines.foreachRDD(rdd -> {
+            JavaRDD<Document> meteoDocuments = rdd.map(line -> {
+                List<String> fields = Arrays.asList(SEPARATOR.split(line));
+                log.info("Linea RDD: " + Arrays.toString(fields.toArray()));
+
+                String id = fields.get(0);
+                String timestamp = fields.get(1);
+                String temperature = fields.get(2);
+                String humidity = fields.get(5);
+                String rain = fields.get(9);
+                String wind_dir = fields.get(10);
+                String wind_speed = fields.get(12);
+                String pressure = fields.get(16);
+                String radiation = fields.get(19);
+
+                Document document = new Document("_id", id);
+                document.put("timestamp", timestamp);
+                document.put("temperature", temperature);
+                document.put("humidity", humidity);
+                document.put("rain", rain);
+                document.put("wind_dir", wind_dir);
+                document.put("wind_speed", wind_speed);
+                document.put("pressure", pressure);
+                document.put("radiation", radiation);
+
+                return document;
+            });
+
+            MongoSpark.save(meteoDocuments);
+        });
+        //meteoLines.print();
+        //meteoFields.print();
 
         // Split suolo fields
-        JavaDStream<String> suoloFields = suoloLines.flatMap(line -> Arrays.asList(SEPARATOR.split(line)).iterator());
-        suoloLines.print();
+        //JavaDStream<String> suoloFields = suoloLines.flatMap(line -> Arrays.asList(SEPARATOR.split(line)).iterator());
+        //suoloLines.print();
 
         jsc.start();
         jsc.awaitTermination();
-
     }
 
 }
